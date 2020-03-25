1e6014838f16601b63c1004c16209ce869ee92d6
cezanne <cezanne199422@gmail.com>
2020-03-25 20:56:25 +0100

    create function from task

--- Arbeitsblatt1/src/app/Aufgaben.java
+++ Arbeitsblatt1/src/app/Aufgaben.java
@@ -1,20 +1,39 @@
+/**
+ * @author Christian Cezanne
+ **/
+
 package app;
 import processing.core.*;
 
 public class Aufgaben extends PApplet {
-    public static void main(String[] args) {
-        PApplet.main("app.Aufgaben");
+  public static void main(String[] args) {
+      PApplet.main("app.Aufgaben");
+  }
+
+  public void settings() {
+
+  }
+
+  public void setup() {
+      Aufgabe1();
+  }
+
+  public void draw() {
+
+  }
+
+  private void Aufgabe1() {
+    int[] lernfeldNoten = {};
+    double summe = 0;
+    double mittelwertArithmetischFeld = 0;
+
+    for (int i = 0; i < lernfeldNoten.length; i++) {
+          summe += lernfeldNoten[i];
     }
+    mittelwertArithmetischFeld = summe / lernfeldNoten.length;
 
-    public void settings() {
-        
-    }
-
-    public void setup() {
-
-    }
-
-    public void draw() {
-
-    }
+    println("Noten: ");
+    println(lernfeldNoten);
+    println("arithmetischer Mittelwert: " + mittelwertArithmetischFeld);
+  }
 }
