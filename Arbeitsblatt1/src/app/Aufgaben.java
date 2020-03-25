6e389d1be3298bfc566bab4cd32b5883db0c99df
Chris <cezanne199422@gmail.com>
2020-03-25 23:40:41 +0100

    add task 3 to file

--- Arbeitsblatt1/src/app/Aufgaben.java
+++ Arbeitsblatt1/src/app/Aufgaben.java
@@ -18,7 +18,8 @@ public class Aufgaben extends PApplet {
   }
 
   public void setup() {
-      Aufgabe1();
+      // Aufgabe1();
+      Aufgabe3();
   }
 
   public void draw() {
@@ -44,5 +45,35 @@ public class Aufgaben extends PApplet {
     println("Noten: ");
     println(lernfeldNoten);
     println("arithmetischer Mittelwert: " + mittelwertArithmetischFeld);
+  }
+
+  private void Aufgabe3() {
+    int[] lernfeldNoten = new int[11];
+    double summe = 0;
+    double mittelwertArithmetischFeld = 0;
+    Scanner noteScanner = new Scanner(System.in); 
+
+    for (int i = 0; i < 11; i++) {
+      println("Bitte geben sie für das Lernfeld-" + (i + 1) + " eine Note ein");
+      lernfeldNoten[i] = noteScanner.nextInt();
+    }
+
+    for (int i = 0; i < lernfeldNoten.length; i++) {
+          summe += lernfeldNoten[i];
+    }
+    mittelwertArithmetischFeld = summe / lernfeldNoten.length;
+
+    println("Noten sind: ");
+    for (int i = 0; i < lernfeldNoten.length; i++) {
+      println("Lernfeld-" + (i + 1) + ": " + lernfeldNoten[i] + "; ");
+    }
+    println("arithmetischer Mittelwert: " + mittelwertArithmetischFeld);
+    
+    println("Beste Note/n");
+    for (int i = 0; i < lernfeldNoten.length; i++) {
+      if(lernfeldNoten[i] == min(lernfeldNoten)) {
+        println("Lernfeld-" + (i + 1) + ": " + lernfeldNoten[i]);
+      }
+    }
   }
 }
