f370594493790d828478772b41f3693ed4aaa38d
cezanne <cezanne199422@gmail.com>
2020-03-25 21:07:25 +0100

    add function for task 1

--- Arbeitsblatt1/src/app/Aufgaben.java
+++ Arbeitsblatt1/src/app/Aufgaben.java
@@ -3,7 +3,10 @@
  **/
 
 package app;
+import java.util.Scanner;
+
 import processing.core.*;
+
 
 public class Aufgaben extends PApplet {
   public static void main(String[] args) {
@@ -23,9 +26,15 @@ public class Aufgaben extends PApplet {
   }
 
   private void Aufgabe1() {
-    int[] lernfeldNoten = {};
+    int[] lernfeldNoten = new int[11];
     double summe = 0;
     double mittelwertArithmetischFeld = 0;
+    Scanner noteScanner = new Scanner(System.in); 
+
+    for (int i = 0; i < 11; i++) {
+      System.out.println("Bitte geben sie für das Lernfeld"+(i+1)+" eine Note ein");
+      lernfeldNoten[i] = noteScanner.nextInt();
+    }
 
     for (int i = 0; i < lernfeldNoten.length; i++) {
           summe += lernfeldNoten[i];
