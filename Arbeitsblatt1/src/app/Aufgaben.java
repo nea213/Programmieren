28868836e4e230213f7f4a9bab02ec13cb6924e0
cezanne <cezanne199422@gmail.com>
2020-03-25 20:47:39 +0100

    become processing to run

---
+++ Arbeitsblatt1/src/app/Aufgaben.java
@@ -0,0 +1,20 @@
+package app;
+import processing.core.*;
+
+public class Aufgaben extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("app.Aufgaben");
+    }
+
+    public void settings() {
+        
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+}
--- Arbeitsblatt1/src/app/Aufgaben1.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-public class Aufgaben1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-    }
-}
