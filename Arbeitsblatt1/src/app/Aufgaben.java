54c9bc6f541ddaca0c218ad2d45b4fef1b15317f
cezanne <cezanne199422@gmail.com>
2020-03-26 07:24:38 +0100

    function to task 4

--- Arbeitsblatt1/src/app/Aufgaben.java
+++ Arbeitsblatt1/src/app/Aufgaben.java
@@ -19,7 +19,8 @@ public class Aufgaben extends PApplet {
 
   public void setup() {
       // Aufgabe1();
-      Aufgabe3();
+      // Aufgabe3();
+      Aufgabe4();
   }
 
   public void draw() {
@@ -51,11 +52,11 @@ public class Aufgaben extends PApplet {
     int[] lernfeldNoten = new int[11];
     double summe = 0;
     double mittelwertArithmetischFeld = 0;
-    Scanner noteScanner = new Scanner(System.in); 
+    Scanner notenScanner = new Scanner(System.in); 
 
     for (int i = 0; i < 11; i++) {
       println("Bitte geben sie für das Lernfeld-" + (i + 1) + " eine Note ein");
-      lernfeldNoten[i] = noteScanner.nextInt();
+      lernfeldNoten[i] = notenScanner.nextInt();
     }
 
     for (int i = 0; i < lernfeldNoten.length; i++) {
@@ -75,5 +76,39 @@ public class Aufgaben extends PApplet {
         println("Lernfeld-" + (i + 1) + ": " + lernfeldNoten[i]);
       }
     }
+  }
+
+  private void Aufgabe4() {
+    int[][] noten = {
+      {2, 30},
+      {3, 60},
+      {4, 100},
+      {1, 100},
+      {2, 20},
+      {5, 100},
+      {3, 100},
+      {1, 40},
+      {2, 40},
+      {3, 40},
+      {2, 20},
+    };
+    double summ = 0;
+    double average = 0;
+    Scanner notenScanner = new Scanner(System.in);
+
+    // for (int i = 0; i < noten.length; i++) {
+    //   println("Bitte geben sie für das Lernfeld-" + (i + 1) + " eine Note ein");
+    //   noten[i][0] = notenScanner.nextInt();
+    // }
+
+    for (int[] note : noten) {
+      double convert = Double.valueOf(note[1]) / 100;
+      summ += note[0] * convert;
+      average += convert;
+    }
+
+    println(summ);
+    println(average);
+    println(summ/noten.length);
   }
 }
