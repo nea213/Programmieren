699256350e9317df011477f6b6fba6277130a717
cezanne <cezanne199422@gmail.com>
2020-03-26 13:40:30 +0100

    init default datas

---
+++ Arbeitsblatt1/src/app/Aufgaben2.java
@@ -0,0 +1,25 @@
+/**
+ * @author Christian Cezanne
+ * 
+ */
+
+package app;
+
+import processing.core.PApplet;
+
+public class Aufgaben2 extends PApplet {
+  public static void main(String[] args) {
+    PApplet.main("app.Aufgaben2");
+  }
+  public void settings() {
+    
+  }
+
+  public void setup() {
+    
+  }
+
+  public void draw() {
+  
+  }
+}
