a34572f17f33ed6113d6b2d12627e027c26ceecc
Chris <cezanne199422@gmail.com>
2020-05-20 12:44:43 +0200

    create default

---
+++ Aufgaben/src/app/pandemie.java
@@ -0,0 +1,20 @@
+package app;
+
+import processing.core.*;
+
+public class pandemie extends PApplet {
+  public static void main(String[] args) {
+    PApplet.main("app.pandemie");
+  }
+
+  public void setup() {   
+  }
+
+  public void settings() {
+    size(100, 100);
+  }
+  
+  public void draw() {
+    ellipse(10, 10, 10, 10);
+  }
+}
