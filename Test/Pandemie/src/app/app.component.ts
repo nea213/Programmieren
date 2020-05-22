d5de7dc970d526e1742f551037bf549f96222737
cezanne <cezanne199422@gmail.com>
2020-05-22 18:04:26 +0200

    create start function and showing

--- Test/Pandemie/src/app/app.component.ts
+++ Test/Pandemie/src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: { class: 'app-root' },
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   days = 0;
+  people = 100;
+  infectChance = 5;
+  infectedPeople: boolean[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this._calcInfected(this.people, this.infectChance);
+  }
+
+  private _calcInfected(people: number, infectChance: number) {
+    this.infectedPeople = [];
+    for (let i = 0; i < people; i++) {
+      if (Math.random() * 100 < infectChance) {
+        this.infectedPeople.push(true);
+      } else {
+        this.infectedPeople.push(false);
+      }
+    }
+  }
+
+  setPeople() {
+    this._calcInfected(this.people, this.infectChance);
+  }
+
+  infectAllPeople(infectedPeople: boolean[]) {
+    if (infectedPeople.filter((x) => !x).length !== 0) {
+      this.days++;
+      setTimeout(() => {
+        for (let i = 0; i < this.infectedPeople.length; i++) {
+          if (!infectedPeople[i]) {
+            continue;
+          }
+          if (i === 0 && infectedPeople[0]) {
+            if (!infectedPeople[i + 1]) {
+              i++;
+              infectedPeople[i] = true;
+            }
+            continue;
+          }
+          if (!infectedPeople[i - 1]) {
+            infectedPeople[i - 1] = true;
+          }
+          if (i + 1 < infectedPeople.length && !infectedPeople[i + 1]) {
+            infectedPeople[i + 1] = true;
+            i++;
+          }
+        }
+        this.infectedPeople = infectedPeople;
+        return this.infectAllPeople(infectedPeople);
+      }, 300);
+    } else {
+    }
+  }
 }
--- Test/Pandemie/src/app/app.module.ts
+++ Test/Pandemie/src/app/app.module.ts
@@ -5,17 +5,33 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { DialogComponent } from './dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DialogComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatIconModule,
     HttpClientModule,
+    CommonModule,
+    MatSliderModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [DialogComponent],
 })
 export class AppModule {
   constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
