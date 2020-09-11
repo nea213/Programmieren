3668233fd7225619db4861080130477614e9421c
Christian <cezanne199422@gmail.com>
2020-09-11 10:15:42 +0200

    create default files

---
+++ C#/CC_HandyApp/CC_HandyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CC_HandyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#/CC_HandyApp/CC_HandyClass/Handy.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CC_HandyClass
+{
+    public class Handy
+    {
+    }
+}
