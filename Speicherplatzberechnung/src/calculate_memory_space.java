6348136e5a42a3f5dbb1a7531cac63bf4be90f7d
cezanne <cezanne199422@gmail.com>
2020-11-08 12:36:55 +0100

    adding some right calculations

--- Speicherplatzberechnung/src/calculate_memory_space.java
+++ Speicherplatzberechnung/src/calculate_memory_space.java
@@ -9,9 +9,14 @@ import java.util.Scanner;
 
 public class calculate_memory_space {
     public static void calculate(int length, int height, int colorType, int compression) {
-        var temp = ((length * height) * colorType) / compression;
+        double bit = ((length * height) * colorType);
+        double Byte = bit / 8;
+        double kib = Byte / 1024;
+        double mib = kib / 1024;
+        double compression_byte = mib / (double) compression;
+        double rounded_value = Math.round(compression_byte * 100.0) / 100.0;
 
-        System.out.println("Die größe ist: " + temp);
+        System.out.println("Der Speicherplatz beträgt: " + rounded_value + " MiB");
     }
 
     public static void main(String[] args) {
@@ -26,24 +31,12 @@ public class calculate_memory_space {
         length = Integer.parseInt(length_height[0]);
         height = Integer.parseInt(length_height[1]);
 
-        while (true) {
-            System.out.println("Bitte geben sie die die Farbiefe in Bytes an: ");
-            colorType = input.nextInt();
-            if (IsPowerOfTwo(colorType)) {
-                break;
-            } else {
-                System.out.println("Es handelt sich hier bei nicht um eine 2-Potenz");
-            }
-        }
+        System.out.println("Bitte geben sie die die Farbiefe in Bytes an: ");
+        colorType = input.nextInt();
 
         System.out.println("Bitte geben sie den Komprimierungsfactor ein: ");
         compression = input.nextInt();
 
         calculate(length, height, colorType, compression);
-    }
-
-    static boolean IsPowerOfTwo(int x)
-    {
-        return (x != 0) && ((x & (x - 1)) == 0);
     }
 }
