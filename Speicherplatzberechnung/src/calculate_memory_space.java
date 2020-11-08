3290c5f16f4a058fa5374d7d9681a24dad21a82b
cezanne <cezanne199422@gmail.com>
2020-11-08 10:40:49 +0100

    some adjustments

--- Speicherplatzberechnung/src/calculate_memory_space.java
+++ Speicherplatzberechnung/src/calculate_memory_space.java
@@ -19,17 +19,17 @@ public class calculate_memory_space {
         int length = 0;
         int height = 0;
         int colorType = 0;
-        int compression = 10;
+        int compression = 0;
 
-        System.out.println("Bitte geben sie die Breite und die Höhe an");
+        System.out.println("Bitte geben sie die Breite und die Höhe an (Bitte mit ',' trennen): ");
         var length_height = input.next().split(",");
         length = Integer.parseInt(length_height[0]);
         height = Integer.parseInt(length_height[1]);
 
-        System.out.println("Bitte geben sie die die Farbiefe");
+        System.out.println("Bitte geben sie die die Farbiefe: ");
         colorType = input.nextInt();
 
-        System.out.println("Bitte geben sie den Komprimierungsfactor ein. (Default: 10)");
+        System.out.println("Bitte geben sie den Komprimierungsfactor ein: ");
         compression = input.nextInt();
 
         calculate(length, height, colorType, compression);
