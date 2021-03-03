68d5fced018edf8da993bec701a35d3146e93df2
cezanne <cezanne199422@gmail.com>
2021-03-03 09:43:34 +0100

    anpassung an änderrungen vom donnerstag

--- Lohnbuchhaltung/src/LohnsteuerGUI.java
+++ Lohnbuchhaltung/src/LohnsteuerGUI.java
@@ -2,8 +2,14 @@ public class LohnsteuerGUI {
     public static void main(String[] args) {
         Personenstammdaten personenstammdaten1 = new Personenstammdaten(1, "Hans", "Peter", "Mussterstraße 43", 1, null, "DE1231451235132");
         Firmenstammdaten firmenstammdaten1 = new Firmenstammdaten("Erste-Straße 1" , 14578456, "Speziall Firma");
+        Firmenstammdaten f1 = new Firmenstammdaten("Oskar-Meixner-Straße 1 68163 Mannheim", 1234567890, "Bilfinger SE");
+        Personenstammdaten p1 = new Personenstammdaten(5184, "Martin", "Carrol", "BlaBla 1 2134 Dummy", 1, "RK", "DE1234567890");
         Verwaltung verwaltung = new Verwaltung();
+
         verwaltung.addStammdaten(firmenstammdaten1, personenstammdaten1);
+        verwaltung.durckeAnmeldung();
+
+        verwaltung.addStammdaten(f1, p1);
         verwaltung.durckeAnmeldung();
     }
 }
--- Lohnbuchhaltung/src/Verwaltung.java
+++ Lohnbuchhaltung/src/Verwaltung.java
@@ -1,17 +1,53 @@
 import java.util.List;
 
 public class Verwaltung {
-    private Firmenstammdaten _firmaData;
-    private Personenstammdaten _persoData;
+    private Firmenstammdaten[] _firmaData;
+    private Personenstammdaten[] _persoData;
 
 
     public void addStammdaten(Firmenstammdaten f, Personenstammdaten p) {
-        this._firmaData = f;
-        this._persoData = p;
+        if (_firmaData == null || _persoData == null) {
+            _firmaData = new Firmenstammdaten[1];
+            _persoData = new Personenstammdaten[1];
+
+            _firmaData[0] = f;
+            _persoData[0] = p;
+        } else {
+            int anzahlFirma = _firmaData.length;
+            int anzahlPerson = _persoData.length;
+
+            Firmenstammdaten[] firm_temp = new Firmenstammdaten[anzahlFirma + 1];
+            Personenstammdaten[] perso_temp = new Personenstammdaten[anzahlPerson + 1];
+
+            for (int i = 0; i < anzahlFirma; i++) {
+                firm_temp[i] = _firmaData[i];
+            }
+            for (int i = 0; i < anzahlPerson; i++) {
+                perso_temp[i] = _persoData[i];
+            }
+
+            firm_temp[anzahlFirma] = f;
+            perso_temp[anzahlPerson] = p;
+
+            _firmaData = firm_temp;
+            _persoData = perso_temp;
+        }
     }
 
     public void durckeAnmeldung() {
-        System.out.println("Firma: " + this._firmaData.get_name() + " - " + this._firmaData.get_addresse() + " - " + this._firmaData.get_IDN());
-        System.out.println("Perosnon: " + this._persoData.get_vorname() + " - " + this._persoData.get_nachname() + " - " + this._persoData.get_adresse() + " - " + this._persoData.get_IBAN() + " - " + this._persoData.get_konfession() + " - " + this._persoData.get_personalnummer() + " - " + this._persoData.get_steuerklasse());
+        for(int i = 0; i < _firmaData.length; i++){
+            System.out.println("----------Firmenstammdaten----------");
+            System.out.println("Name: " + _firmaData[i].get_name());
+            System.out.println("Adresse: " + _firmaData[i].get_addresse());
+            System.out.println("Steuer-ID: " + _firmaData[i].get_IDN());
+            System.out.println("----------Personenstammdaten---------");
+            System.out.println("Personalnummer: " + _persoData[i].get_personalnummer());
+            System.out.println("Name: " + _persoData[i].get_vorname() + " " + _persoData[i].get_nachname());
+            System.out.println("Adresse: " + _persoData[i].get_adresse());
+            System.out.println("Steuerklasse: " + _persoData[i].get_steuerklasse());
+            System.out.println("Konfession: " + _persoData[i].get_konfession());
+            System.out.println("IBAN: " + _persoData[i].get_IBAN());
+            System.out.println();
+        }
     }
 }
